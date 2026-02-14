de3dde7c8659bd855b4b085ff69e7cb5ff8c5c46
Artur Kotov <semanticparadox@gmail.com>
2026-02-13 22:24:30 -0500

    fix

--- apps/panel/src/scripts.rs
+++ apps/panel/src/scripts.rs
@@ -5,11 +5,6 @@ use rust_embed::RustEmbed;
 pub struct Scripts;
 
 impl Scripts {
-    pub fn get_setup_node_script() -> Option<String> {
-        Self::get("setup_node.sh")
-            .and_then(|file| String::from_utf8(file.data.to_vec()).ok())
-    }
-
     pub fn get_universal_install_script() -> Option<String> {
         Self::get("install.sh")
             .and_then(|file| String::from_utf8(file.data.to_vec()).ok())
